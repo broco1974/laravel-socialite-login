f6cc765bc9020d8199004ffa33898953d4acbdfd
xdimension <xdim2272@gmail.com>
2015-08-08 23:10:40 +0700

    move catch exception

--- src/Auth/AuthenticateUser.php
+++ src/Auth/AuthenticateUser.php
@@ -48,16 +48,16 @@ class AuthenticateUser {
 				}
 				throw new Exception('Access denied.');
 			}
-		}
-		catch (Exception $e)
-		{
-			return $listener->loginFailure($provider, $e);
-		}
 
-		$user = $this->users->findOrCreateUser($provider, $userData);
+            $user = $this->users->findOrCreateUser($provider, $userData);
 
-		$remember = true;
-		$this->auth->login($user, $remember);
+            $remember = true;
+            $this->auth->login($user, $remember);
+        }
+        catch (Exception $e)
+        {
+            return $listener->loginFailure($provider, $e);
+        }
 
 		return $listener->loginSuccess($user);
 	}
